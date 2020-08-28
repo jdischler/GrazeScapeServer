69cc17431b8fbf8a0dd4a70117526d3302780412
Jeff Dischler <jdischler.72@gmail.com>
2020-08-28 16:44:42 -0500

    UI container set up for animal grazing module

--- app/models/transform/Clamp.java
+++ app/models/transform/Clamp.java
@@ -2,14 +2,28 @@ package models.transform;
 
 import query.Layer_Float;
 
-// example: "clamp=?/56"
-//------------------------------------------------------------
+/**
+ *  Provides a clamping data transform
+ *   
+ *  example in model file: 
+ *  "clamp=?/56"
+ *  example in Java code: 
+ *  Clamp c = new Clamp("?/56");
+ *///------------------------------------------------------------
 public class Clamp implements Transform {
 	
 	public Boolean mMinClamped= false, mMaxClamped = false;
 	public Float mMinClamp, mMaxClamp;
 
-	//------------------------------------------------------------
+	/**
+	 *  Constructor
+	 * 
+	 * ```java
+	 * // Create a clamp object that doesn't test a minimum but ensures the an output value never exceeds 56.  
+	 * Clamp c = new Clamp("?/56");
+	 *  ```
+	 *  @param String Clamp specification in "<min>/<max>" format, where <min> and <max> are <Float> types. A '?' can be subsitited instead if the given element doesn't need to be clamped 
+	 *///------------------------------------------------------------
 	public Clamp(String value) {
 		// Split and minimally sanitize
 		String clmp[] = value.split("/");
@@ -26,7 +40,17 @@ public class Clamp implements Transform {
 		}
 	}
 	
-	//------------------------------------------------------------
+	/** Apply the configured clamp to the requested input
+	 * 
+	 * ```java
+	 * // Create a clamp object that doesn't test a minimum but ensures the an output value never exceeds 56.  
+	 * Clamp c = new Clamp("?/56");
+	 * Float result = c.apply(64.3f); // result will equal 56.0
+	 * result = c.apply(-64.3f); // result will equal -64.3
+	 *  ```
+	 *  @param Float the value to clamp
+	 *  @return The input value as clamped by the specification from the constructor 
+	 *///------------------------------------------------------------
 	public final Float apply(Float input) {
 		if (Layer_Float.isNoDataValue(input)) return input;
 		if (mMinClamped && input < mMinClamp) input = mMinClamp;
--- public/js/state/scenario/AnimalDialog.js
+++ public/js/state/scenario/AnimalDialog.js
@@ -8,42 +8,114 @@ Ext.define('DSS.state.scenario.AnimalDialog', {
 	autoDestroy: false,
 	closeAction: 'hide',
 	constrain: true,
-//	floating: true,
-//	shadow: false,
 	modal: true,
-	margin: 32,
 	width: 480,
-	minHeight: 320,
+	minHeight: 80,
+	minWidth: 320,
 	resizable: true,
 	
-	title: 'Configure animals',
+	title: 'Configure Animals',
 	
 //	style: 'border-radius: 2px; box-shadow: 0 4px 8px rgba(0,0,0,0.5);',
-	layout: DSS.utils.layout('hbox', 'start'),
+	layout: DSS.utils.layout('vbox', 'start', 'stretch'),
 	
 	//--------------------------------------------------------------------------
 	initComponent: function() {
 		let me = this;
 
 		Ext.applyIf(me, {
-			defaults: {
- 				xtype: 'button',
-				toggleGroup: 'field-shape-mode',
-				padding: '4 0 0 0',
-				height: 30,
-				allowDepress: false,
-				frame: false
-			},
 			items: [{
-				text: 'Work in progress',
-				tooltip: 'TBD',
-				width: 140, 
+				xtype: 'component',
+				padding: 4,
+				cls: 'information med-text',
+				html: 'Click to choose the types of animals present at this operation'
+					
 			},{
-				html: '<i class="fas fa-search"></i>',
-				tooltip: 'Activate Inspector <i class="fas fa-search accent-text"></i> mode',
-				width: 48,
-				pressed: true
+				xtype: 'container',
+				layout: DSS.utils.layout('hbox', 'center'),
+				defaults: {
+					xtype: 'button',
+					margin: '8 4',
+					minWidth: 100,
+					enableToggle: true
+				},
+				items: [{
+					text: 'Dairy',
+					toggleHandler: function(self, pressed) {
+						
+						let container = me.down("#bloof-1");
+						if (pressed) {
+							container.setHeight(0);
+							container.setVisible(true)
+							container.animate({
+								dynamic: true,
+								to: {
+									height: 230
+								}
+							});
+						} 
+						else {
+							me.setHeight(null)
+							container.animate({
+								dynamic: true,
+								to: {
+									height: 0
+								},
+								callback: function() {
+									container.setVisible(false);
+								}
+							});
+						}
+					}
+				},{
+					text: 'Beef',
+					toggleHandler: function(self, pressed) {
+						me.setHeight(null)
+						me.down("#bloof-2").setVisible(pressed)
+					}
+				}]
+			},{
+				xtype: 'container',
+				//width: '100%',
+				//flex: 1,
+				margin: 8,
+				scrollable: 'y',
+				layout: DSS.utils.layout('vbox', 'start', 'stretch'),
+				items: [{
+					itemId: 'bloof-1',
+					xtype: 'component',
+					hidden: true,
+					height: 230,
+					style: 'background-color: red'
+				},{
+					itemId: 'bloof-2',
+					xtype: 'component',
+					hidden: true,
+					height: 230,
+					style: 'background-color: yellow'
+				}]	
 			}]
+			// Add Dairy
+			// 	number of lactating cows
+			// 	number of dry cows
+			//  number of heifers
+			//	number of youngstock
+			//	- Lactating Cows
+			//		Grazing Time (hours/day)
+			//		Grazing Period (months/year)
+			//		Confined Period (months/year)
+			//	- Non-Lactating Cows
+			//		Grazing Time (hours/day)
+			//		Grazing Period (months/year)
+			//		Confined Period (months/year)
+			// Add Beef
+			//	number of beef cow
+			//	number of stockers
+			//	number of finishing
+			//	- Beef
+			//		Grazing Time (hours/day)
+			//		Grazing Period (months/year)
+			//		Confined Period (months/year)
 		});
 		
 		me.callParent(arguments);
