91841d081f4f3083de1d73e0c1c08aeeae6fa765
Jeff Dischler <jdischler.72@gmail.com>
2020-09-30 17:07:27 -0500

    remove unneeded imports

--- app/db/FieldGeometry.java
+++ app/db/FieldGeometry.java
@@ -1,13 +1,9 @@
 package db;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
